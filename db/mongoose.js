8b77783a3c736705af00bf2c988040e586694d96
Mustafa Temel <mustafa.temel@borusanotomotiv.com>
2021-02-05 11:55:59 +0300

    ...

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://mucosmos:iMe6E00zqEAxfby7G7Sy3VgKFef7qIcFuMNl6ZgAIZq2Ofbp9mKtJS5LbFJGTLUQob1HPNojFGtpETkH0SvkkA==@mucosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@mucosmos@', {
+mongoose.connect('mongodb://mucosmos:iMe6E00zqEAxfby7G7Sy3VgKFef7qIcFuMNl6ZgAIZq2Ofbp9mKtJS5LbFJGTLUQob1HPNojFGtpETkH0SvkkA==@mucosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@mucosmos@&retrywrites=false', {
     useNewUrlParser: true,
     socketTimeoutMS: 5000,
     useUnifiedTopology: true,
